d6773d6fe9aa4dcc4cdfe20eeadde12762a95003
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-08 19:53:47 +0500

    Add src\components\Footer.js

---
+++ src\components\Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+  return (
+    <footer>
+      <p>&copy; 2023 Ecommerce Website</p>
+    </footer>
+  );
+}
+
+export default Footer;
